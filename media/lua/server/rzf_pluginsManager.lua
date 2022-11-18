b459910380a789f34dd25cff11ab720e9463d211
theCrius <claudio.vallesi@gmail.com>
2022-11-18 18:38:36 +0000

    adjust logic to accommodate different triggers

--- media/lua/server/rzf_pluginsManager.lua
+++ media/lua/server/rzf_pluginsManager.lua
@@ -1,7 +1,8 @@
 local utilities = require('rzf_utilities')
+local presetManager = require('rzf_presetManager')
 
 -- Global table for plugins
-RZF_PLUGINS = {}
+RZF_PLUGINS = RZF_PLUGINS or {}
 
 local pluginsManager = {}
 
@@ -30,8 +31,20 @@ end
 pluginsManager.overrideWithPlugin = function(detectedPreset)
     local activatedPlugins = pluginsManager.getEnabledPlugins()
     for i, pluginData in pairs(activatedPlugins) do
-        if (pluginData.triggerFn()) then
-            detectedPreset = pluginData.presetName
+        if(pluginData.triggerMode == nil) then
+            if (pluginData.triggerFn()) then
+                detectedPreset = pluginData.presetName
+            end
+        end
+        if(pluginData.triggerMode == 'threshold') then
+            if (pluginData.triggerFn(pluginData.threshold)) then
+                detectedPreset = pluginData.presetName
+            end
+        end
+        if(pluginData.triggerMode == 'schedule') then
+            if (pluginData.triggerFn(pluginData.schedule)) then
+                detectedPreset = pluginData.presetName
+            end
         end
     end
     return detectedPreset
@@ -44,27 +57,6 @@ pluginsManager.getPresetDatabyName = function(presetName)
             return pluginData.presetData
         end
     end
-end
-
--- This function is just here to show how to add a new plugin to the global plugins table
-local function exampleAddPluginTemplate()
-    local pluginDataTemplate = {
-        presetName = "exampleTime",              -- give a unique name to your preset, possibly with the suffix `Time`
-        enabled = false,                         -- determine if this plugin is enabled or disable
-        triggerFn = function() return true end,  -- this function must return true/false (boolean) and will be used to determine if the preset must activate
-        presetData = {                           -- follow this structure to specify the distribution data for your preset
-            crawler = 20.0,
-            shambler = 30.0,
-            fastShambler = 30.0,
-            sprinter = 20.0,
-            fragile = 50.0,
-            normal = 30.0,
-            tough = 20.0,
-            smart = 10.0
-        }
-    }
-
-    table.insert(RZF_PLUGINS, pluginDataTemplate) -- use this to add your table created above to the global RZF_PLUGINS table
 end
 
 return pluginsManager
