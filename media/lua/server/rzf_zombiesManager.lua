1e3e4f6821639d3449a8822da948ce43acfa2665
theCrius <claudio.vallesi@gmail.com>
2023-03-01 16:11:43 +0000

    disabling skip for recycled zombies

--- media/lua/server/rzf_zombiesManager.lua
+++ media/lua/server/rzf_zombiesManager.lua
@@ -70,11 +70,12 @@ zombiesManager.updateZombie = function(zombie, distribution, speedType, cognitio
     -- from _intended_ default state (i.e. RZ happened to assign it to default bucket) to _unintended_
     -- default state (i.e. RZ would not assign it to the default bucket, even though it's the same zombie)
     -- see IsoZombie::resetForReuse and VirtualZombieManager::createRealZombieAlways for more info
-    local shouldSkip = speedTypeVal == modData.speed
-                        and cognitionVal == modData.cognition
-                        and crawlingVal == modData.crawling
-                        and math.abs(squareXVal - modData.x) <= 20
-                        and math.abs(squareYVal - modData.y) <= 20
+    -- local shouldSkip = speedTypeVal == modData.speed
+    --                     and cognitionVal == modData.cognition
+    --                     and crawlingVal == modData.crawling
+    --                     and math.abs(squareXVal - modData.x) <= 20
+    --                     and math.abs(squareYVal - modData.y) <= 20
+    local shouldSkip = false
 
     -- NOTE (RandomZombie - Belette) we check for square to avoid crash in Java engine postupdate calls later
     if shouldSkip or (not square) then
