a9f020f203e80b960ed0a3dc486d6828796c3a86
theCrius <claudio.vallesi@gmail.com>
2024-07-20 00:11:05 +0100

    Formatting cleanup

--- Contents/mods/RandomZombiesDayAndNight/media/lua/server/rzf_zombiesManager.lua
+++ Contents/mods/RandomZombiesDayAndNight/media/lua/server/rzf_zombiesManager.lua
@@ -100,121 +100,121 @@ end
 
 -- Update a single zombie according to parameters passed
 zombiesManager.updateZombie = function(zombie, distribution, speedType, cognition, memory, sight, hearing)
-    local modData = zombie:getModData()
-    local speedTypeVal = getClassFieldVal(zombie, speedType)
-    local cognitionVal = getClassFieldVal(zombie, cognition)
-    local memoryVal = getClassFieldVal(zombie, memory)
-    local sightVal = getClassFieldVal(zombie, sight)
-    local hearingVal = getClassFieldVal(zombie, hearing)
-    local crawlingVal = zombie:isCrawling()
-    local square = zombie:getCurrentSquare()
-    local squareXVal = square and square:getX() or 0
-    local squareYVal = square and square:getY() or 0
+  local modData = zombie:getModData()
+  local speedTypeVal = getClassFieldVal(zombie, speedType)
+  local cognitionVal = getClassFieldVal(zombie, cognition)
+  local memoryVal = getClassFieldVal(zombie, memory)
+  local sightVal = getClassFieldVal(zombie, sight)
+  local hearingVal = getClassFieldVal(zombie, hearing)
+  local crawlingVal = zombie:isCrawling()
+  local square = zombie:getCurrentSquare()
+  local squareXVal = square and square:getX() or 0
+  local squareYVal = square and square:getY() or 0
 
-    -- print("[RZF] ===================")
-    -- print("[RZF] speed", SPEED_DEFAULT, speedTypeVal)
-    -- print("[RZF] cognition", COGNITION_DEFAULT, cognitionVal)
-    -- print("[RZF] memory", MEMORY_DEFAULT, memoryVal)
-    -- print("[RZF] sight", SIGHT_DEFAULT, sightVal)
-    -- print("[RZF] hearing", HEARING_DEFAULT, hearingVal)
-    -- print("[RZF] ===================")
+  -- print("[RZF] ===================")
+  -- print("[RZF] speed", SPEED_DEFAULT, speedTypeVal)
+  -- print("[RZF] cognition", COGNITION_DEFAULT, cognitionVal)
+  -- print("[RZF] memory", MEMORY_DEFAULT, memoryVal)
+  -- print("[RZF] sight", SIGHT_DEFAULT, sightVal)
+  -- print("[RZF] hearing", HEARING_DEFAULT, hearingVal)
+  -- print("[RZF] ===================")
 
-    -- NOTE (RandomZombie - Belette) we have to include X and Y in the check to catch zombies that have been recycled
-    -- from _intended_ default state (i.e. RZ happened to assign it to default bucket) to _unintended_
-    -- default state (i.e. RZ would not assign it to the default bucket, even though it's the same zombie)
-    -- see IsoZombie::resetForReuse and VirtualZombieManager::createRealZombieAlways for more info
-    -- local shouldSkip = speedTypeVal == modData.speed
-    --                     and cognitionVal == modData.cognition
-    --                     and crawlingVal == modData.crawling
-    --                     and math.abs(squareXVal - modData.x) <= 20
-    --                     and math.abs(squareYVal - modData.y) <= 20
-    local shouldSkip = false
+  -- NOTE (RandomZombie - Belette) we have to include X and Y in the check to catch zombies that have been recycled
+  -- from _intended_ default state (i.e. RZ happened to assign it to default bucket) to _unintended_
+  -- default state (i.e. RZ would not assign it to the default bucket, even though it's the same zombie)
+  -- see IsoZombie::resetForReuse and VirtualZombieManager::createRealZombieAlways for more info
+  -- local shouldSkip = speedTypeVal == modData.speed
+  --                     and cognitionVal == modData.cognition
+  --                     and crawlingVal == modData.crawling
+  --                     and math.abs(squareXVal - modData.x) <= 20
+  --                     and math.abs(squareYVal - modData.y) <= 20
+  local shouldSkip = false
 
-    -- NOTE (RandomZombie - Belette) we check for square to avoid crash in Java engine postupdate calls later
-    if shouldSkip or (not square) then
-      return true
-    end
-
-    local zid = utilities.zombieID(zombie)
-    local slice = utilities.hashToSlice(zid)
-
-    -- update speed
-    if slice < distribution.crawler then
-      if not zombie:isCrawling() then
-        zombie:toggleCrawling()
-        zombie:setCanWalk(false);
-        zombie:setFallOnFront(true)
-      end
-    elseif slice < distribution.shambler then
-      zombiesManager.updateSpeed(zombie, SPEED_SHAMBLER, speedTypeVal)
-      if zombie:isCrawling() and zombiesManager.shouldBeStanding(zombie) then
-        zombie:toggleCrawling()
-        zombie:setCanWalk(true);
-      end
-    elseif slice < distribution.fastShambler then
-        zombiesManager.updateSpeed(zombie, SPEED_FAST_SHAMBLER, speedTypeVal)
-      if zombie:isCrawling() and zombiesManager.shouldBeStanding(zombie) then
-        zombie:toggleCrawling()
-        zombie:setCanWalk(true);
-      end
-    elseif slice < distribution.sprinter then
-        zombiesManager.updateSpeed(zombie, SPEED_SPRINTER, speedTypeVal)
-      if zombie:isCrawling() and zombiesManager.shouldBeStanding(zombie) then
-        zombie:toggleCrawling()
-        zombie:setCanWalk(true);
-      end
-    -- fallback to 'proper zombies' in case something is missed
-    else
-      zombiesManager.updateSpeed(zombie, SPEED_DEFAULT, speedTypeVal)
-      if zombie:isCrawling() and zombiesManager.shouldBeStanding(zombie) then
-        zombie:toggleCrawling()
-        zombie:setCanWalk(true);
-      end
-    end
-
-    -- update smart zombies
-    if distribution.smart > 0 then
-      zid = utilities.hash(zid)
-      local slice2 = utilities.hashToSlice(zid)
-      if slice2 < distribution.smart then
-        zombiesManager.updateCognition(zombie, COGNITION_DOORS, cognitionVal, cognition)
-        zombiesManager.updateMemory(zombie, MEMORY_LONG, memoryVal, memory)
-      else
-        zombiesManager.updateCognition(zombie, COGNITION_DEFAULT, cognitionVal, cognition)
-        zombiesManager.updateMemory(zombie, MEMORY_DEFAULT, memoryVal, memory)
-      end
-    end
-
-    -- update toughness
-    if distribution.normal ~= 100 then
-      -- Only update health of zombies that are not on fire and not being attacked.
-      -- The getAttackedBy() check returns truthy if the last damage the zombie took was from a player...
-      -- however if the zombie is on fire, then any fire damage will cause the "AttackedBy" status to expire,
-      -- so we need to also check for burning, otherwise burning zombies will regen health.
-      -- See https://github.com/theCrius/project-zomboid-random-zombies-full/issues/9 for more info.
-      if not zombie:getAttackedBy() and not zombie:isOnFire() then
-        zid = utilities.hash(zid)
-        local slice3 = utilities.hashToSlice(zid)
-        local health = 0.1 * ZombRand(4)
-        if slice3 < distribution.fragile  then
-          health = health + 0.5
-        elseif slice3 < distribution.tough then
-          health = health + 1.5
-        else
-          health = health + 3.5
-        end
-        zombie:setHealth(health)
-      end
-    end
-
-    modData.speed = getClassFieldVal(zombie, speedType)
-    modData.cognition = getClassFieldVal(zombie, cognition)
-    modData.crawling = zombie:isCrawling()
-    modData.x = squareXVal
-    modData.y = squareYVal
-
-    return false
+  -- NOTE (RandomZombie - Belette) we check for square to avoid crash in Java engine postupdate calls later
+  if shouldSkip or (not square) then
+    return true
   end
+
+  local zid = utilities.zombieID(zombie)
+  local slice = utilities.hashToSlice(zid)
+
+  -- update speed
+  if slice < distribution.crawler then
+    if not zombie:isCrawling() then
+      zombie:toggleCrawling()
+      zombie:setCanWalk(false);
+      zombie:setFallOnFront(true)
+    end
+  elseif slice < distribution.shambler then
+    zombiesManager.updateSpeed(zombie, SPEED_SHAMBLER, speedTypeVal)
+    if zombie:isCrawling() and zombiesManager.shouldBeStanding(zombie) then
+      zombie:toggleCrawling()
+      zombie:setCanWalk(true);
+    end
+  elseif slice < distribution.fastShambler then
+      zombiesManager.updateSpeed(zombie, SPEED_FAST_SHAMBLER, speedTypeVal)
+    if zombie:isCrawling() and zombiesManager.shouldBeStanding(zombie) then
+      zombie:toggleCrawling()
+      zombie:setCanWalk(true);
+    end
+  elseif slice < distribution.sprinter then
+      zombiesManager.updateSpeed(zombie, SPEED_SPRINTER, speedTypeVal)
+    if zombie:isCrawling() and zombiesManager.shouldBeStanding(zombie) then
+      zombie:toggleCrawling()
+      zombie:setCanWalk(true);
+    end
+  -- fallback to 'proper zombies' in case something is missed
+  else
+    zombiesManager.updateSpeed(zombie, SPEED_DEFAULT, speedTypeVal)
+    if zombie:isCrawling() and zombiesManager.shouldBeStanding(zombie) then
+      zombie:toggleCrawling()
+      zombie:setCanWalk(true);
+    end
+  end
+
+  -- update smart zombies
+  if distribution.smart > 0 then
+    zid = utilities.hash(zid)
+    local slice2 = utilities.hashToSlice(zid)
+    if slice2 < distribution.smart then
+      zombiesManager.updateCognition(zombie, COGNITION_DOORS, cognitionVal, cognition)
+      zombiesManager.updateMemory(zombie, MEMORY_LONG, memoryVal, memory)
+    else
+      zombiesManager.updateCognition(zombie, COGNITION_DEFAULT, cognitionVal, cognition)
+      zombiesManager.updateMemory(zombie, MEMORY_DEFAULT, memoryVal, memory)
+    end
+  end
+
+  -- update toughness
+  if distribution.normal ~= 100 then
+    -- Only update health of zombies that are not on fire and not being attacked.
+    -- The getAttackedBy() check returns truthy if the last damage the zombie took was from a player
+    -- however if the zombie is on fire, then any fire damage will cause the "AttackedBy" status to expire,
+    -- so we need to also check for burning, otherwise burning zombies will "regen" health.
+    -- See https://github.com/theCrius/project-zomboid-random-zombies-full/issues/9 for more info.
+    if not zombie:getAttackedBy() and not zombie:isOnFire() then
+      zid = utilities.hash(zid)
+      local slice3 = utilities.hashToSlice(zid)
+      local health = 0.1 * ZombRand(4)
+      if slice3 < distribution.fragile  then
+        health = health + 0.5
+      elseif slice3 < distribution.tough then
+        health = health + 1.5
+      else
+        health = health + 3.5
+      end
+      zombie:setHealth(health)
+    end
+  end
+
+  modData.speed = getClassFieldVal(zombie, speedType)
+  modData.cognition = getClassFieldVal(zombie, cognition)
+  modData.crawling = zombie:isCrawling()
+  modData.x = squareXVal
+  modData.y = squareYVal
+
+  return false
+end
 
 -- Update all zombies in a cell according to the active distribution
 zombiesManager.updateAllZombies = function(zombieDistribution, updateFrequency)
